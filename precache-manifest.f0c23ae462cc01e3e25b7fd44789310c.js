abd7da0c793c5f8e8bf7738c586010ca8868cae5
nguyenhuusy <52096108+nguyenhuusy@users.noreply.github.com>
2020-02-25 21:14:30 +0700

    Updates

---
+++ precache-manifest.f0c23ae462cc01e3e25b7fd44789310c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f32c933fe1f438f956562298551ea95",
+    "url": "/ColorfulTiles/index.html"
+  },
+  {
+    "revision": "df393cf9ac661387cf87",
+    "url": "/ColorfulTiles/static/css/main.fcecc63c.chunk.css"
+  },
+  {
+    "revision": "9f95d7e09f8de94aba50",
+    "url": "/ColorfulTiles/static/js/2.aa2d4ea5.chunk.js"
+  },
+  {
+    "revision": "9fcbc669d68edcb506b730c9ae8a8e55",
+    "url": "/ColorfulTiles/static/js/2.aa2d4ea5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df393cf9ac661387cf87",
+    "url": "/ColorfulTiles/static/js/main.045d4cdc.chunk.js"
+  },
+  {
+    "revision": "c749ff970680235044a6",
+    "url": "/ColorfulTiles/static/js/runtime-main.b2279f0e.js"
+  }
+]);
